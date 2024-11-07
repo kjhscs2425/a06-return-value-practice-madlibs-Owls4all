ff49ac1e171df41d2dcbf5a757abb3474bc3253e
owls4all <owls4all@gmail.com>
2024-11-07 11:30:48 -0800

    produced comedic nonsense

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-adjective0 = input("Give me an adjective: ")
-noun0 = input("Give me a noun: ")
+from utility import *
+inputs =[ask('a unit of time'),
+         ask('an adjective'),
+         ask('a noun'),
+         ask('a pronoun(posessive)'),
+         ask('another adjective'), 
+         ask('a plural noun'),
+         ask('a verb (past)'),
+         ask('a verb'),
+         ask('an adjective'),
+         ask('a noun'),
+         ask('a pronoun(posessive)'),
+         ask('another noun'),
+         ask('an emotion'),
+         ask('a verb (past)')]
+        
 
-story = f"There are many {adjective0} ways to choose a/n {noun0} to read."
+story=f'''Once, many {inputs[0]} ago, there was a/n {inputs[1]} {inputs[2]}.
+The {inputs[2]} and {inputs[3]} {inputs[4]} {inputs[5]} {inputs[6]} to {inputs[7]} a/n {inputs[8]} {inputs[9]}.
+the {inputs[9]}, however, became aware of {inputs[10]} {inputs[11]}, and became {inputs[12]}.
+When the {inputs[8]} {inputs[9]} met the {inputs[2]} and {inputs[3]} {inputs[4]} {inputs[5]}, they {inputs[13]}
+'''
 print(story)
